25d4515075c032f77a15192db88a3abbd8722067
Aidin Teymouri <aidintv@gmail.com>
2025-03-12 14:23:11 +1000

    Sign-in form

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,11 +1,11 @@
-// import { ShoppingCart, UserIcon } from "lucide-react";
+import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "./menu";
 
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-// import ModeToggle from "./mode-toggle";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -26,7 +26,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
-          {/* <ModeToggle />
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart />
@@ -38,7 +38,7 @@ const Header = () => {
               <UserIcon />
               Sign In
             </Link>
-          </Button> */}
+          </Button>
         </div>
         <Menu />
       </div>
